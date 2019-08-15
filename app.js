084c85774d9bab6dd7d850435e4429fe54675b11
tehLurdUfEngmer <pierre.pagnoux@protonmail.com>
2019-08-15 17:12:08 +0200

    update : new field size of flats available now

---
+++ app.js
@@ -0,0 +1,190 @@
+
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const ent = require('ent');
+const MongoClient = require('mongodb').MongoClient;
+const math = require('mathjs');
+const parseJson = require('parse-json');
+const cov = require('compute-covariance');
+//var Highcharts = require('highcharts');
+//require('highcharts/modules/exporting.js')(Highcharts);
+
+const urldb = "mongodb://localhost:27017/";
+
+// page par default : index.html
+app.get('/', function (req, res) {
+	res.setHeader('Content-Type','text/html');
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+pente = function(array2d){
+	// Calcul de la pente ramenée au Semestre
+	var array1= [];var array2 = [];
+	array2d.forEach(function(e,index,objet){
+		// separation des X et des Y
+		array1.push(e[0]);
+		array2.push(e[1]);
+	});
+	// formule : a = cov(X,Y)/Var(X)
+	const pente = cov(array1,array2)[0][1]/math.variance(array1);
+	//console.log(pente*15778800000);
+	return pente*15778800000;
+}
+
+// Evenements webSocket
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+
+
+	// requete
+	socket.on('request', function (obj) {
+		// log de la requete
+		/*
+		attributs de l'objets requête :
+					- typlog: type de logement (Maison, Appartement etc.)
+					- cp : codePostal ou debut de Code Postal
+					- smin : surface Habitable minimale
+					- smax : surface Habitable MAximale
+					- nbpce : nombre de pièces principales
+		*/
+
+		//
+		if(obj.nbpce == 5){
+			// regexp : >= 5
+			var ssrqtNbPce = new RegExp('^(([5-9])|([1-9]\d{1,}))$');
+		}else if(obj.nbpce == 0){
+			var ssrqtNbPce = new RegExp('^[0-9]{1,}$')
+		}
+		else{
+			var ssrqtNbPce = new RegExp('^'+obj.nbpce+'$');
+		}
+
+		// gestion type logement :
+		if(obj.typlog == "a"){
+			// autre que Maison ou Appartement
+			var ssrqtLog = {$not: { $in:["1","2"]}};
+		}else if(obj.typlog == "t"){
+			// tout type de logement
+			var ssrqtLog = new RegExp('^[0-9]{0,}$');
+		}else{
+			var ssrqtLog = obj.typlog;
+		}
+
+		//contruction du regexp : Code Postal
+		regex = new RegExp('^'+obj.cp+'.*');
+
+		// requete mongodb
+		MongoClient.connect(urldb,{ useNewUrlParser: true }, function(err, db) {
+			  if (err) throw err;
+			  // construction de la requete :
+
+			  query = {nbPiecePrincipales : ssrqtNbPce,codeTypeLocal: ssrqtLog, codePostal : regex, surfaceReelle : {"$lt" : parseFloat(obj.smax), "$gt" : parseFloat(obj.smin)}};
+			  console.log(query);
+
+			  // database local
+				var dbo = db.db("local");
+			  // collection immobilier
+			  dbo.collection("immobilier").find(query).toArray(function(err, result) {
+				if (err) throw err;
+				//console.log(result.length);
+				db.close();
+
+				var prix_maisons = [];
+				var prix_apparts = [];
+				var prix_autres = [];
+				// découpage par categorie de logement
+				for (var n = 0; n < result.length; n++){
+					if(result[n].typeLocal == "Maison"){
+						prix_maisons.push([new Date(result[n].dateMutation).getTime(),result[n].prixM2Bati]);
+					}else if(result[n].typeLocal == "Appartement"){
+						prix_apparts.push([new Date(result[n].dateMutation).getTime(),result[n].prixM2Bati]);
+					}else{
+						prix_autres.push([new Date(result[n].dateMutation).getTime(),result[n].prixM2Bati]);
+					}
+				}
+
+				//calcul pente
+				var pente_apparts,pente_autres,pente_maisons;
+				if(prix_maisons.length > 1){
+					var reg_maisons = true;
+					pente_maisons = pente(prix_maisons);
+				}else{
+					var reg_maisons = false;
+					pente_maisons = null;
+				}
+				if(prix_apparts.length > 1){
+					var reg_apparts = true;
+					pente_apparts = pente(prix_apparts);
+				}else{
+					var reg_apparts = false;
+					pente_apparts = null;
+				}
+				if(prix_autres.length > 1){
+					var reg_autres = true;
+					pente_autres = pente(prix_autres);
+				}else{
+					var reg_autres = false;
+					pente_autres = null;
+				}
+
+				//console.log('pente maison : '+pente_maisons);
+				//console.log('pente appart : '+pente_apparts);
+				//console.log('pente autre : '+pente_autres);
+				// envoi vers le front
+				socket.emit("data",{dataHouse: prix_maisons, dataFlat: prix_apparts, dataOther: prix_autres, regressions: [reg_maisons,reg_apparts,reg_autres], pentes: [pente_maisons,pente_apparts,pente_autres]});
+
+				// calcul mediane
+				var boxPlotdata = [];
+				var boxPlotCategories = [];
+				var myArray = [];
+				const sixMonths = 15778800000;
+				//var q1, median, q3 = 0;
+				var semestre = [];
+				// decoupage par semestre
+
+
+				for (var ts = new Date('2014').getTime(); ts < new Date().getTime(); ts+=sixMonths){
+					result.forEach(function (obj){
+						if(new Date(obj.dateMutation).getTime() > ts && new Date(obj.dateMutation).getTime() < (ts+sixMonths) && obj.prixM2Bati != null){
+							myArray.push(obj.prixM2Bati);
+						}
+					});
+
+					if(myArray.length > 2){
+						//console.log(myArray);
+						//console.log('3*ecarttype :'+3*math.std(myArray));
+						var std = math.std(myArray);
+						var mean = math.mean(myArray);
+						//console.log('mean: '+mean);
+
+						// suppression des valeurs aberrantes
+						myArray.forEach(function (e,index,array){
+							if(Math.abs(e - mean) > 2*std){// 2*std = 0.9545
+								//console.log(array[index]);
+								array.splice(index,1);
+							}
+						});
+						// q1 q2 q3 du semestre en cours
+						if(myArray.length > 2){
+
+							var quantiles = (math.quantileSeq(myArray,[0.1,0.25,0.5,0.75,0.9]));
+
+							boxPlotCategories.push(new Date(ts).toISOString());
+							boxPlotdata.push(quantiles);
+						}
+					}
+					myArray = [];
+				}
+				//console.log(quantiles);
+				//console.log(boxPlotdata);
+				//console.log(boxPlotCategories);
+				socket.emit("stats_basic",{categories : boxPlotCategories, data : boxPlotdata});
+		  });
+		});
+	});
+});
+
+
+server.listen(8080);
