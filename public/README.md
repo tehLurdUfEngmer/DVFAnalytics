73288ad65bd302206a7af21ab700049c3b7e262a
TehLurdUfEngmer <pierre.pagnoux@protonmail.com>
2019-08-20 20:04:20 +0200

    ajout d'un element googleMaps

--- public/README.md
+++ public/README.md
@@ -1,36 +1,36 @@
-***DVF - Solution Immobilière***
+# DVF - Solution Immobilière***
 
-** Author **
+### Author
 TehLurdUfEngmer
 
-** Description **
+## Description
 This application was designed to run hard statistical and data analysis on DVF from France open data files
 
-** Data Sources **
+## Data Sources
 opendata.gouv.fr
 
-** Git Help **
-# check Git version :
+## Git Help
+** check Git version : **
 git --version
 
-# set user name and email
+** set user name and email **
 git config --global user.name "-name-"
 git config --global user.email "-mail-"
 
-# initialize Git project
+** initialize Git project **
 cd pathToTheProjectToInitalize
 git init
 
-# check if there is modifications to commit
+** check if there is modifications to commit **
 git status
 
-# add or update a file to Git Project
+** add or update a file to Git Project **
 git add subPath/file.txt
 
-# commit changes
+** commit changes **
 git commit -m "description of changes"
 
-# get historics of past commits:
+** get historics of past commits: **
 git log
 
 # create a git branch (project copy for working)
@@ -54,3 +54,6 @@ git remote add origin https://github.com/tehLurdUfEngmer/DVFAnalytics.git
 
 # deposit project to gitHub
 git push origin master
+
+# pull from gitHub to branch master
+git pull origin master
---
+++ public/map.js
@@ -0,0 +1,60 @@
+
+
+let $map = document.querySelector('#map')
+
+class GoogleMap{
+
+  constructor(){
+    this.map = null
+    this.bounds = null
+  }
+
+
+  /*
+  - charge la carte sur un element HTML
+  - @param {HTML Element} element
+  */
+  async load(element){
+    return new Promise((resolve,reject)=>{
+      $script('https://maps.googleapis.com/maps/api/js', function(){
+        let center = {lat:-25.363, lng:131.044}
+        let map = new google.maps.Map(element,{zoom: 4, center: center})
+      })
+      resolve()
+    })
+
+  }
+
+  /**
+  @param : {String} lat : Latitude
+  @param : {String} lon : Longitude
+  @param : {String} txt : Texte
+  */
+  addMarker(lat, lon, txt){
+    let point = new google.maps.LatLng(lat,lon)
+    let marker = new google.maps.Marker({
+      position: {lat:lat, lng:lon},
+      map: this.map
+    })
+    this.bounds(point)
+  }
+
+  /**
+  * Centrer la map
+  */
+  centerMap(){
+    this.map.panToBounds(this.bounds)
+    this.map.fitBounds(this.bounds)
+  }
+}
+
+const initMap = async function(){
+  let map = new GoogleMap()
+  await map.load($map)
+  Array.from(document.querySelectorAll('.js-coordonates')).forEach(function(item){
+    map.addMarker(item.dataSet.lat, item.dataSet.lon, item.dataSet.txt + '€')
+  })
+}
+if($map !== null){
+  initMap()
+}
